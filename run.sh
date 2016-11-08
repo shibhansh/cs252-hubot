6b6bb64ca79579e56c35cf40a8489cc711a499af
pratikab <pratikab@iitk.ac.in>
2016-11-08 17:15:05 +0530

    EMAIL/FETCH

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ echo "Password: "
 stty -echo
 read  passd
 stty echo
-env HUBOT_SLACK_TOKEN=xoxb-98837274053-FGvXnxcjJP7BIBYpZx1wibA1 HUBOT_EMAIL_ID=bhangalepratik8@gmail.com HUBOT_EMAIL_USER=$user HUBOT_EMAIL_PWD=$passd ./bin/hubot -a slack
+env HUBOT_SLACK_TOKEN=xoxb-98837274053-FGvXnxcjJP7BIBYpZx1wibA1 HUBOT_EMAIL_ID=bhangalepratik8@gmail.com HUBOT_EMAIL_USER=$user HUBOT_EMAIL_PWD=$passd GMAIL_USER=bhangalepratik8@gmail.com GMAIL_PASSWORD=$passd GMAIL_LABEL=inbox GMAIL_FETCH_INTERVAL=1 ./bin/hubot -a slack
